b04429c4b057f6a9fe6d438c8d1f4f465901125a
Anay Chopade <shrichopade@gmail.com>
2021-02-23 08:25:08 +0000

    Manage Profile screens

--- app/screens/chemist/profile/ChangePassword.js
+++ app/screens/chemist/profile/ChangePassword.js
@@ -4,11 +4,24 @@ import { Button } from 'native-base';
 
 import pageStyles from '../../common/PageStyle.js';
 import ChemistFooter from '../ChemistFooter';
+import AppTextInput from '../../../components/AppTextInput';
+
 
 export default class ChangePassword extends React.Component {
 
     constructor() {
         super();
+        this.state = {
+          oldPassword: '',
+          password: '',
+          confirmPassword: ''
+        }
+    }
+
+    inputValueUpdate = (val, prop) => {
+        const state = this.state;
+        state[prop] = val;
+        this.setState(state);
     }
 
     render() {
@@ -17,6 +30,35 @@ export default class ChangePassword extends React.Component {
                 <View style={pageStyles.screen}>
                     <View style={pageStyles.body}>
                         <Text style={styles.pageTitle}>CHANGE PASSWORD</Text>
+                        <AppTextInput
+                            value={this.state.passcode}
+                            onChangeText={(val) => this.inputValueUpdate(val, 'oldPassword')}
+                            leftIcon="file-code"
+                            placeholder="Enter current password"
+                            autoCapitalize="none"
+                            keyboardType="phone-pad"
+                            textContentType="oneTimeCode"
+                            />
+                        <AppTextInput
+                            value={this.state.password}
+                            onChangeText={(val) => this.inputValueUpdate(val, 'password')}
+                            leftIcon="lock"
+                            placeholder="Enter new password"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            secureTextEntry
+                            textContentType="password"
+                            />
+                        <AppTextInput
+                            value={this.state.confirmPassword}
+                            onChangeText={(val) => this.inputValueUpdate(val, 'confirmPassword')}
+                            leftIcon="lock"
+                            placeholder="Confirm new password"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            secureTextEntry
+                            textContentType="password"
+                            />
                         <Button success style={styles.buttonStyle} title="Submit" 
                             onPress={() => this.props.navigation.navigate('ChemistConfirmChanges')}>
                             <Text style={styles.buttonText}>Submit</Text>
--- app/screens/chemist/profile/ManagePersonalInfo.js
+++ app/screens/chemist/profile/ManagePersonalInfo.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, View, StyleSheet } from 'react-native';
 import { Button } from 'native-base';
+import { RadioButton, Text, Checkbox  } from 'react-native-paper';
 
 import pageStyles from '../../common/PageStyle.js';
 import ChemistFooter from '../ChemistFooter';
+import AppTextInput from '../../../components/AppTextInput';
+import AppDateTextMask from '../../../components/AppDateTextMask';
 
 export default class ManagePersonalInfo extends React.Component {
 
     constructor() {
         super();
+        this.state = {
+            givenNames: '',
+            surname: '',
+            gender: 'M',
+            dateOfBirth: '',
+            mobileNumber: '',
+            aadharCard: '',
+            yourStatus: 'P',
+            registrationNumber: '',
+            checked: 'unchecked'
+        }
+    }
+
+    inputValueUpdate = (val, prop) => {
+        const state = this.state;
+        state[prop] = val;
+        this.setState(state);
     }
 
     render() {
@@ -17,6 +37,50 @@ export default class ManagePersonalInfo extends React.Component {
                 <View style={pageStyles.screen}>
                     <View style={pageStyles.body}>
                         <Text style={styles.pageTitle}>MANAGE PERSONAL INFORMATION</Text>
+                        <AppTextInput
+                            value={this.state.givenName}
+                            onChangeText={(val) => this.inputValueUpdate(val, 'givenName')}
+                            leftIcon="face"
+                            placeholder="Enter First names"
+                            autoCapitalize="words"
+                            keyboardType="default"
+                            textContentType="givenName"
+                        />
+                        <AppTextInput
+                            value={this.state.surname}
+                            onChangeText={(val) => this.inputValueUpdate(val, 'surname')}
+                            leftIcon="face"
+                            placeholder="Enter Surname"
+                            autoCapitalize="words"
+                            keyboardType="default"
+                            textContentType="familyName"
+                        />
+                        <AppDateTextMask
+                            type={'datetime'}
+                            leftIcon="file-code"
+                            options={{format: 'DD/MM/YYYY'}}
+                            value={this.state.dateOfBirth}
+                            placeholder="Enter Date of Birth (DD/MM/YYYY)"
+                            onChangeText={(val) => this.inputValueUpdate(val, 'dateOfBirth')}
+                        />                        
+                        <AppTextInput
+                            value={this.state.mobileNumber}
+                            onChangeText={(val) => this.inputValueUpdate(val, 'mobileNumber')}
+                            leftIcon="file-code"
+                            placeholder="Enter mobile number"
+                            autoCapitalize="none"
+                            keyboardType="phone-pad"
+                            textContentType="telephoneNumber"
+                        />
+                        <AppTextInput
+                            value={this.state.aadharCard}
+                            onChangeText={(val) => this.inputValueUpdate(val, 'aadharCard')}
+                            leftIcon="file-code"
+                            placeholder="Enter Aadhar number"
+                            autoCapitalize="none"
+                            keyboardType="phone-pad"
+                            textContentType="creditCardNumber"
+                        />
                         <Button success style={styles.buttonStyle} title="Submit" 
                             onPress={() => this.props.navigation.navigate('ChemistConfirmChanges')}>
                             <Text style={styles.buttonText}>Submit</Text>
