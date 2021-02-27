41451cd55993518d597da00ced6f4bc9190af23b
Anay Chopade <shrichopade@gmail.com>
2021-02-27 01:17:03 +0000

    Patient Page

--- app/data/patientorders.js
+++ app/data/patientorders.js
@@ -3,16 +3,16 @@ export default [
     key: "ORD001",
     status_image: require("../../assets/images/pending.png"),
     status: 'Pending',
-    chemist: "Suresh Medicals",
-    doctor: "Dr R Joshi",
-    due_date: "15/03/2021"
+    chemist: "Chemist: Suresh Medicals",
+    doctor: "Doctor: Dr R Joshi",
+    due_date: "Due: 15/03/2021"
   },
   {
       key: "ORD005",
       status_image: require("../../assets/images/complete.png"),
       status: 'Complete',
-      chemist: "Mahalaxmi Medicals",
-      doctor: "Dr K Nene",
-      due_date: "02/03/2021"
+      chemist: "Chemist: Mahalaxmi Medicals",
+      doctor: "Doctor: Dr K Nene",
+      due_date: "Delivered: 02/03/2021"
   }     
   ];
