533dfbf901afde0f0092ae8bd44b59dc1d188f58
Anay Chopade <shrichopade@gmail.com>
2021-02-27 08:16:36 +0000

    Manage Doc/Chemists

--- app/screens/patient/profile/ManageChemists.js
+++ app/screens/patient/profile/ManageChemists.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
-import { Button } from 'native-base';
+import { SafeAreaView, View, Image, StyleSheet } from 'react-native';
+import { Button, Card, CardItem, Text } from 'native-base';
 
 import pageStyles from '../../common/PageStyle.js';
 import PatientFooter from '../PatientFooter';
@@ -27,18 +27,73 @@ export default class ManageChemists extends React.Component {
                 <View style={pageStyles.screen}>
                     <View style={pageStyles.body}>
                         <Text style={styles.pageTitle}>MANAGE MY CHEMISTS</Text>
-                        <AppTextInput
-                            value={this.state.passcode}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'passcode')}
-                            leftIcon="file-code"
-                            placeholder="Enter chemist"
-                            autoCapitalize="none"
-                            keyboardType="phone-pad"
-                            textContentType="oneTimeCode"
-                            />
+                        <View style={styles.cardRowStyle}>
+                            <Card style={styles.cardStyle}>
+                                <CardItem body bordered style={styles.cardHeader}>
+                                    <View style={styles.cardHeaderContainer}>
+                                        <Image
+                                            source={
+                                                require('../../../../assets/images/drprofile.png')
+                                            }
+                                            style={styles.profileImage}
+                                        />
+                                        <Text style={styles.level0Text}>Suresh Medicals</Text>
+                                    </View>
+                                </CardItem>
+                                <CardItem body bordered style={styles.cardBody}>
+                                    <View style={styles.cardBodyContainer}>
+                                        <Text style={styles.level1Text}>Kothrud, Pune</Text>
+                                        <Text style={styles.level2Text}>M - 9876545678</Text>
+                                        <Text style={styles.level2Text}>E - s.kute@gmail.com</Text>
+                                    </View>
+                                </CardItem>
+                            </Card>
+                            <Card style={styles.cardStyle}>
+                                <CardItem body bordered style={styles.cardHeader}>
+                                    <View style={styles.cardHeaderContainer}>
+                                        <Image
+                                            source={
+                                                require('../../../../assets/images/chemistprofile.png')
+                                            }
+                                            style={styles.profileImage}
+                                        />
+                                        <Text style={styles.level0Text}>Mahalaxmi Medicals</Text>
+                                    </View>
+                                </CardItem>
+                                <CardItem body bordered style={styles.cardBody}>
+                                    <View style={styles.cardBodyContainer}>
+                                        <Text style={styles.level1Text}>Kothrud Depot, Pune</Text>
+                                        <Text style={styles.level2Text}>M - 8762345678</Text>
+                                        <Text style={styles.level2Text}>E - laxmimedical@gmail.com</Text>
+                                    </View>
+                                </CardItem>
+                            </Card>
+                        </View>
+                        <View style={styles.cardRowStyle}>
+                            <Card style={styles.cardStyle}>
+                                <CardItem body bordered style={styles.cardHeader}>
+                                    <View style={styles.cardHeaderContainer}>
+                                        <Image
+                                            source={
+                                                require('../../../../assets/images/chemistprofile.png')
+                                            }
+                                            style={styles.profileImage}
+                                        />
+                                        <Text style={styles.level0Text}>Solkar Pharmacy</Text>
+                                    </View>
+                                </CardItem>
+                                <CardItem body bordered style={styles.cardBody}>
+                                    <View style={styles.cardBodyContainer}>
+                                        <Text style={styles.level1Text}>Deccan, Pune</Text>
+                                        <Text style={styles.level2Text}>M - 9876545678</Text>
+                                        <Text style={styles.level2Text}>E - sphrma@yahoo.com</Text>
+                                    </View>
+                                </CardItem>
+                            </Card>
+                        </View>
                         <Button success style={styles.buttonStyle} title="Submit" 
                             onPress={() => this.props.navigation.navigate('PatientConfirmChanges')}>
-                            <Text style={styles.buttonText}>Submit</Text>
+                            <Text style={styles.buttonText}>Add New Chemist</Text>
                         </Button>
                     </View>
                     <View style={pageStyles.footer}>
@@ -55,9 +110,70 @@ const styles = StyleSheet.create({
         padding: 10,
         fontSize: 20,
         marginTop: 10,
-        marginBottom: 10,
+        marginBottom: 2,
         fontWeight: 'bold',
         backgroundColor: '#93cf96'
+    },
+    cardRowStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    cardStyle: {
+        width: '50%',
+    },
+    cardHeader: {
+        marginVertical: 1,
+        marginBottom: 1,
+        backgroundColor: '#C8E7C9',
+    },
+    cardHeaderContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    cardBody: {
+        marginVertical: 1,
+        marginBottom: 1,
+        backgroundColor: '#E4F3E4',
+    },
+    cardBodyContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+    },
+    profileImage: {
+        width: 40,
+        height: 40,
+        borderRadius: 40 / 2,
+        marginLeft: 0,
+    },
+    level0Text: {
+        fontFamily: 'Arial',
+        fontSize: 16,
+        fontWeight: '600',
+        marginLeft: 5,
+        marginRight: 5,
+        marginVertical: 0,
+        marginBottom: 0,
+        flex: 1, 
+        flexWrap: 'wrap',
+    },
+    level1Text: {
+        fontFamily: 'Arial',
+        fontSize: 16,
+        fontWeight: '500',
+        marginLeft: 0,
+        marginRight: 2,
+        marginBottom: 5,
+    },
+    level2Text: {
+        fontFamily: 'Arial',
+        fontSize: 14,
+        fontWeight: '400',
+        marginLeft: 0,
+        marginRight: 2,
+        marginBottom: 5,
     },
     buttonStyle: {
         borderRadius: 10,
@@ -65,8 +181,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         padding: 5,
+        marginTop: 5,
         marginBottom: 5,
-        width: '20%'
+        width: '50%'
     },
     buttonText: {
         color: 'white',
--- app/screens/patient/profile/ManageDoctors.js
+++ app/screens/patient/profile/ManageDoctors.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
-import { Button } from 'native-base';
+import { SafeAreaView, View, Image, StyleSheet } from 'react-native';
+import { Button, Card, CardItem, Text } from 'native-base';
 
 import pageStyles from '../../common/PageStyle.js';
 import PatientFooter from '../PatientFooter';
@@ -27,18 +27,93 @@ export default class ManageDoctors extends React.Component {
                 <View style={pageStyles.screen}>
                     <View style={pageStyles.body}>
                         <Text style={styles.pageTitle}>MANAGE MY DOCTORS</Text>
-                        <AppTextInput
-                            value={this.state.passcode}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'passcode')}
-                            leftIcon="file-code"
-                            placeholder="Enter doctor"
-                            autoCapitalize="none"
-                            keyboardType="phone-pad"
-                            textContentType="oneTimeCode"
-                            />
+                        <View style={styles.cardRowStyle}>
+                            <Card style={styles.cardStyle}>
+                                <CardItem body bordered style={styles.cardHeader}>
+                                    <View style={styles.cardHeaderContainer}>
+                                        <Image
+                                            source={
+                                                require('../../../../assets/images/drprofile.png')
+                                            }
+                                            style={styles.profileImage}
+                                        />
+                                        <Text style={styles.level0Text}>Dr Santosh Pote</Text>
+                                    </View>
+                                </CardItem>
+                                <CardItem body bordered style={styles.cardBody}>
+                                    <View style={styles.cardBodyContainer}>
+                                        <Text style={styles.level1Text}>Pote Clinic, Pune</Text>
+                                        <Text style={styles.level2Text}>M - 9876545678</Text>
+                                        <Text style={styles.level2Text}>E - poteclinic@gmail.com</Text>
+                                    </View>
+                                </CardItem>
+                            </Card>
+                            <Card style={styles.cardStyle}>
+                                <CardItem body bordered style={styles.cardHeader}>
+                                    <View style={styles.cardHeaderContainer}>
+                                        <Image
+                                            source={
+                                                require('../../../../assets/images/drprofile.png')
+                                            }
+                                            style={styles.profileImage}
+                                        />
+                                        <Text style={styles.level0Text}>Dr Rahul Joshi</Text>
+                                    </View>
+                                </CardItem>
+                                <CardItem body bordered style={styles.cardBody}>
+                                    <View style={styles.cardBodyContainer}>
+                                        <Text style={styles.level1Text}>Joshi Hospital, Pune</Text>
+                                        <Text style={styles.level2Text}>M - 8762345678</Text>
+                                        <Text style={styles.level2Text}>E - r.joshi@gmail.com</Text>
+                                    </View>
+                                </CardItem>
+                            </Card>
+                        </View>
+                        <View style={styles.cardRowStyle}>
+                            <Card style={styles.cardStyle}>
+                                <CardItem body bordered style={styles.cardHeader}>
+                                    <View style={styles.cardHeaderContainer}>
+                                        <Image
+                                            source={
+                                                require('../../../../assets/images/drprofile.png')
+                                            }
+                                            style={styles.profileImage}
+                                        />
+                                        <Text style={styles.level0Text}>Dr Ram Nene</Text>
+                                    </View>
+                                </CardItem>
+                                <CardItem body bordered style={styles.cardBody}>
+                                    <View style={styles.cardBodyContainer}>
+                                        <Text style={styles.level1Text}>Nene Hospital, Pune</Text>
+                                        <Text style={styles.level2Text}>M - 9876545678</Text>
+                                        <Text style={styles.level2Text}>E - rnene@yahoo.com</Text>
+                                    </View>
+                                </CardItem>
+                            </Card>
+                            <Card style={styles.cardStyle}>
+                                <CardItem body bordered style={styles.cardHeader}>
+                                    <View style={styles.cardHeaderContainer}>
+                                        <Image
+                                            source={
+                                                require('../../../../assets/images/patientprofile.png')
+                                            }
+                                            style={styles.profileImage}
+                                        />
+                                        <Text style={styles.level0Text}>Dr Sujata Sane</Text>
+                                    </View>
+                                </CardItem>
+                                <CardItem body bordered style={styles.cardBody}>
+                                    <View style={styles.cardBodyContainer}>
+                                        <Text style={styles.level1Text}>SS Hospital, Pune</Text>
+                                        <Text style={styles.level2Text}>M - 8762345678</Text>
+                                        <Text style={styles.level2Text}>E - ssane@gmail.com</Text>
+                                    </View>
+                                </CardItem>
+                            </Card>
+                        </View>
                         <Button success style={styles.buttonStyle} title="Submit" 
                             onPress={() => this.props.navigation.navigate('PatientConfirmChanges')}>
-                            <Text style={styles.buttonText}>Submit</Text>
+                            <Text style={styles.buttonText}>Add New Doctor</Text>
                         </Button>
                     </View>
                     <View style={pageStyles.footer}>
@@ -55,9 +130,70 @@ const styles = StyleSheet.create({
         padding: 10,
         fontSize: 20,
         marginTop: 10,
-        marginBottom: 10,
+        marginBottom: 2,
         fontWeight: 'bold',
         backgroundColor: '#93cf96'
+    },
+    cardRowStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    cardStyle: {
+        width: '50%',
+    },
+    cardHeader: {
+        marginVertical: 1,
+        marginBottom: 1,
+        backgroundColor: '#C8E7C9',
+    },
+    cardHeaderContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    cardBody: {
+        marginVertical: 1,
+        marginBottom: 1,
+        backgroundColor: '#E4F3E4',
+    },
+    cardBodyContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+    },
+    profileImage: {
+        width: 40,
+        height: 40,
+        borderRadius: 40 / 2,
+        marginLeft: 0,
+    },
+    level0Text: {
+        fontFamily: 'Arial',
+        fontSize: 16,
+        fontWeight: '600',
+        marginLeft: 5,
+        marginRight: 5,
+        marginVertical: 0,
+        marginBottom: 0,
+        flex: 1, 
+        flexWrap: 'wrap',
+    },
+    level1Text: {
+        fontFamily: 'Arial',
+        fontSize: 16,
+        fontWeight: '500',
+        marginLeft: 0,
+        marginRight: 2,
+        marginBottom: 5,
+    },
+    level2Text: {
+        fontFamily: 'Arial',
+        fontSize: 14,
+        fontWeight: '400',
+        marginLeft: 0,
+        marginRight: 2,
+        marginBottom: 5,
     },
     buttonStyle: {
         borderRadius: 10,
@@ -65,8 +201,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         padding: 5,
+        marginTop: 5,
         marginBottom: 5,
-        width: '20%'
+        width: '50%'
     },
     buttonText: {
         color: 'white',
