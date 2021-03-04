b559b3af185100dc3ee3b1b4855022db4378c174
Anay Chopade <shrichopade@gmail.com>
2021-03-04 19:05:33 +0000

    Validation msg Reset Password

--- app/screens/common/RequestOTP.js
+++ app/screens/common/RequestOTP.js
@@ -1,16 +1,27 @@
+'use strict';
 import React from 'react';
 import { SafeAreaView, View, StyleSheet } from 'react-native';
+import { Text } from 'native-base';
+import ValidationComponent from 'react-native-form-validator';
 
 import pageStyles from './PageStyle.js'
 import AppButton from '../../components/AppButton';
 import AppTextInput from '../../components/AppTextInput';
 import DefaultFooter from './DefaultFooter'
 
-export default class RequestOTP extends React.Component {
-    constructor() {
-      super();
+export default class RequestOTP extends ValidationComponent {
+    constructor(props) {
+      super(props);
       this.state = {
         username: ''
+      }
+    }
+
+    requestOTP = () => {
+      this._validateInputs(); 
+
+      if(this.getErrorMessages().length == 0) {
+        this.props.navigation.navigate('ResetPassword')
       }
     }
   
@@ -20,10 +31,11 @@ export default class RequestOTP extends React.Component {
       this.setState(state);
     }
   
-    validateInput() {
-      if(this.state.username === '') {
-        alert('User name is mandatory')
-      }
+    _validateInputs() {
+      // Call ValidationComponent validate method
+      this.validate({
+        username: {required: true, email: true}
+      });
     }
   
     render() {
@@ -33,16 +45,31 @@ export default class RequestOTP extends React.Component {
                 <View style={pageStyles.body}>
                     <AppTextInput
                         value={this.state.username}
-                        onChangeText={(val) => this.inputValueUpdate(val, 'username')}
+                        onChangeText={(username) => {
+                          this.setState({ username },
+                              () => {
+                                  this.validate({
+                                      username: { required: true, email: true },
+                                  })
+                                }
+                              )                                    
+                          }
+                        }
                         leftIcon="email-open"
                         placeholder="Enter username"
                         autoCapitalize="none"
                         keyboardType="email-address"
                         textContentType="emailAddress"
                         />
+                        {this.isFieldInError('username') 
+                            && this.getErrorsInField('username').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }
                     <View style={styles.buttonContainer}>    
                         <AppButton title="Send Code" 
-                            onPress={() => this.props.navigation.navigate('ResetPassword')} />
+                            onPress={() => this.requestOTP() } />
                     </View>
                 </View>
                 <View style={pageStyles.footer}>
@@ -59,5 +86,20 @@ const styles = StyleSheet.create({
         marginVertical: 15,
         justifyContent: 'center',
         alignItems: 'center'
-    }
+    },
+    errorMsgText: {
+      fontFamily: 'Arial',
+      color: 'red',
+      fontSize: 14,
+      marginLeft: 15,
+      marginBottom: 10,
+    },
+    errorImage: {
+        width: 14,
+        height: 14,
+        borderRadius: 14 / 2,
+        marginTop: 2,
+        marginRight: 5,
+        alignSelf: 'center'
+    },
 });
--- app/screens/common/ResetPassword.js
+++ app/screens/common/ResetPassword.js
@@ -1,19 +1,30 @@
+'use strict';
 import React from 'react';
 import { SafeAreaView, View, StyleSheet } from 'react-native';
+import { Text } from 'native-base';
+import ValidationComponent from 'react-native-form-validator';
 
 import pageStyles from './PageStyle.js'
 import AppButton from '../../components/AppButton';
 import AppTextInput from '../../components/AppTextInput';
 import DefaultFooter from './DefaultFooter'
 
-export default class ResetPassword extends React.Component {
+export default class ResetPassword extends ValidationComponent {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
           passcode: '',
           password: '',
           confirmPassword: ''
+        }
+    }
+
+    resetPassword = () => {
+        this._validateInputs(); 
+  
+        if(this.getErrorMessages().length == 0) {
+          this.props.navigation.navigate('SignIn')
         }
     }
 
@@ -23,6 +34,15 @@ export default class ResetPassword extends React.Component {
         this.setState(state);
     }
 
+    _validateInputs() {
+        // Call ValidationComponent validate method
+        this.validate({
+            passcord: {required: true, numbers: true, minlength: 6, maxlength: 6},
+            password: {required: true, minlength: 3, maxlength: 8},
+            confirmPassword: {equalPassword : this.state.password}
+        });
+    }
+
     render() {
         return (
             <SafeAreaView  style={pageStyles.container}>
@@ -30,16 +50,40 @@ export default class ResetPassword extends React.Component {
                     <View style={pageStyles.body}>
                         <AppTextInput
                             value={this.state.passcode}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'passcode')}
+                            onChangeText={(passcode) => {
+                                this.setState({ passcode },
+                                    () => {
+                                        this.validate({
+                                            passcode: { required: true, numbers: true, minlength: 6, maxlength: 6 },
+                                        })
+                                      }
+                                    )                                    
+                                }
+                            }
                             leftIcon="file-code"
                             placeholder="Enter passcode"
                             autoCapitalize="none"
                             keyboardType="phone-pad"
                             textContentType="oneTimeCode"
                             />
+                        {this.isFieldInError('passcode') 
+                            && this.getErrorsInField('passcode').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }
                         <AppTextInput
                             value={this.state.password}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'password')}
+                            onChangeText={(password) => {
+                                this.setState({ password },
+                                    () => {
+                                        this.validate({
+                                            password: { required: true, minlength: 3, maxlength: 8 },
+                                        })
+                                      }
+                                    )                                    
+                                }
+                            }
                             leftIcon="lock"
                             placeholder="Enter password"
                             autoCapitalize="none"
@@ -47,9 +91,24 @@ export default class ResetPassword extends React.Component {
                             secureTextEntry
                             textContentType="password"
                             />
+                        {this.isFieldInError('password') 
+                            && this.getErrorsInField('password').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }
                         <AppTextInput
                             value={this.state.confirmPassword}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'confirmPassword')}
+                            onChangeText={(confirmPassword) => {
+                                this.setState({ confirmPassword },
+                                    () => {
+                                        this.validate({
+                                            confirmPassword: { equalPassword : this.state.password },
+                                        })
+                                      }
+                                    )                                    
+                                }
+                            }
                             leftIcon="lock"
                             placeholder="Confirm password"
                             autoCapitalize="none"
@@ -57,9 +116,15 @@ export default class ResetPassword extends React.Component {
                             secureTextEntry
                             textContentType="password"
                             />
+                        {this.isFieldInError('confirmPassword') 
+                            && this.getErrorsInField('confirmPassword').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }
                         <View style={styles.buttonContainer}>    
                             <AppButton title="Reset Password" 
-                                onPress={() => this.props.navigation.navigate('SignIn')} />
+                                onPress={() => this.resetPassword() } />
                         </View>
                     </View>
                     <View style={pageStyles.footer}>
@@ -76,5 +141,20 @@ const styles = StyleSheet.create({
         marginVertical: 15,
         justifyContent: 'center',
         alignItems: 'center'
-    }
+    },
+    errorMsgText: {
+        fontFamily: 'Arial',
+        color: 'red',
+        fontSize: 14,
+        marginLeft: 15,
+        marginBottom: 10,
+    },
+    errorImage: {
+        width: 14,
+        height: 14,
+        borderRadius: 14 / 2,
+        marginTop: 2,
+        marginRight: 5,
+        alignSelf: 'center'
+    },
 });
