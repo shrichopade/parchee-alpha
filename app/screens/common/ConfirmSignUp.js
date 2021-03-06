878a54703c6d5f4abb35244d1a3683afd0f07e0d
Anay Chopade <shrichopade@gmail.com>
2021-03-06 07:10:32 +0000

    Validation msg Sign Up

--- app/screens/common/ConfirmSignUp.js
+++ app/screens/common/ConfirmSignUp.js
@@ -1,20 +1,31 @@
+'use strict';
 import React from 'react';
 import { SafeAreaView, View, StyleSheet } from 'react-native';
+import { Text } from 'native-base';
+import ValidationComponent from 'react-native-form-validator';
 
 import pageStyles from './PageStyle.js'
 import AppButton from '../../components/AppButton';
 import AppTextInput from '../../components/AppTextInput';
 import DefaultFooter from './DefaultFooter'
 
-export default class ConfirmSignUp extends React.Component {
+export default class ConfirmSignUp extends ValidationComponent {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
           passcode: '',
           username: '',
           password: '',
           confirmPassword: ''
+        }
+    }
+
+    confirmSignUp = () => {
+        this._validateInputs(); 
+  
+        if(this.getErrorMessages().length == 0) {
+          this.props.navigation.navigate('SignIn')
         }
     }
 
@@ -24,6 +35,16 @@ export default class ConfirmSignUp extends React.Component {
         this.setState(state);
     }
 
+    _validateInputs() {
+        // Call ValidationComponent validate method
+        this.validate({
+            passcord: {required: true, numbers: true, minlength: 6, maxlength: 6},
+            username: {required: true, email: true},
+            password: {required: true, minlength: 3, maxlength: 8},
+            confirmPassword: {equalPassword : this.state.password}
+        });
+    }
+
     render() {
         return (
             <SafeAreaView  style={pageStyles.container}>
@@ -31,25 +52,63 @@ export default class ConfirmSignUp extends React.Component {
                     <View style={pageStyles.body}>
                         <AppTextInput
                             value={this.state.passcode}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'passcode')}
+                            onChangeText={(passcode) => {
+                                this.setState({ passcode },
+                                    () => {
+                                        this.validate({
+                                            passcode: { required: true, numbers: true, minlength: 6, maxlength: 6 },
+                                        })
+                                      }
+                                    )                                    
+                                }
+                            }
                             leftIcon="file-code"
                             placeholder="Enter passcode"
                             autoCapitalize="none"
                             keyboardType="phone-pad"
                             textContentType="oneTimeCode"
                             />
+                        {this.isFieldInError('passcode') 
+                            && this.getErrorsInField('passcode').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }
                         <AppTextInput
                             value={this.state.username}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'username')}
+                            onChangeText={(username) => {
+                                this.setState({ username },
+                                    () => {
+                                        this.validate({
+                                            username: { required: true, email: true },
+                                        })
+                                    }
+                                )}
+                            }
                             leftIcon="email-open"
                             placeholder="Enter email address"
                             autoCapitalize="none"
                             keyboardType="email-address"
                             textContentType="emailAddress"
                         />
+                        {this.isFieldInError('username') 
+                            && this.getErrorsInField('username').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }
                         <AppTextInput
                             value={this.state.password}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'password')}
+                            onChangeText={(password) => {
+                                this.setState({ password },
+                                    () => {
+                                        this.validate({
+                                            password: { required: true, minlength: 3, maxlength: 8 },
+                                        })
+                                      }
+                                    )                                    
+                                }
+                            }
                             leftIcon="lock"
                             placeholder="Enter password"
                             autoCapitalize="none"
@@ -57,9 +116,24 @@ export default class ConfirmSignUp extends React.Component {
                             secureTextEntry
                             textContentType="password"
                             />
+                        {this.isFieldInError('password') 
+                            && this.getErrorsInField('password').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }
                         <AppTextInput
                             value={this.state.confirmPassword}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'confirmPassword')}
+                            onChangeText={(confirmPassword) => {
+                                this.setState({ confirmPassword },
+                                    () => {
+                                        this.validate({
+                                            confirmPassword: { equalPassword : this.state.password },
+                                        })
+                                      }
+                                    )                                    
+                                }
+                            }
                             leftIcon="lock"
                             placeholder="Confirm password"
                             autoCapitalize="none"
@@ -67,9 +141,15 @@ export default class ConfirmSignUp extends React.Component {
                             secureTextEntry
                             textContentType="password"
                             />
+                        {this.isFieldInError('confirmPassword') 
+                            && this.getErrorsInField('confirmPassword').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }
                         <View style={styles.buttonContainer}>    
                             <AppButton title="Register" 
-                                onPress={() => this.props.navigation.navigate('SignIn')} />
+                                onPress={() => this.confirmSignUp()} />
                         </View>
                     </View>
                     <View style={pageStyles.footer}>
@@ -86,5 +166,20 @@ const styles = StyleSheet.create({
         marginVertical: 15,
         justifyContent: 'center',
         alignItems: 'center'
-    }
+    },
+    errorMsgText: {
+        fontFamily: 'Arial',
+        color: 'red',
+        fontSize: 14,
+        marginLeft: 15,
+        marginBottom: 10,
+    },
+    errorImage: {
+        width: 14,
+        height: 14,
+        borderRadius: 14 / 2,
+        marginTop: 2,
+        marginRight: 5,
+        alignSelf: 'center'
+    },
 });
--- app/screens/common/SignUp.js
+++ app/screens/common/SignUp.js
@@ -1,23 +1,32 @@
+'use strict';
 import React from 'react';
 import { SafeAreaView, View, StyleSheet } from 'react-native';
 import { RadioButton, Text, Checkbox  } from 'react-native-paper';
+import ValidationComponent from 'react-native-form-validator';
 
 import pageStyles from './PageStyle.js'
 import AppButton from '../../components/AppButton';
 import AppTextInput from '../../components/AppTextInput';
-import AppDateTextMask from '../../components/AppDateTextMask';
 import DefaultFooter from './DefaultFooter'
 
-export default class SignUp extends React.Component {
+export default class SignUp extends ValidationComponent {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            givenNames: '',
+            givenName: '',
             surname: '',
             mobileNumber: '',
             referalId: '',
             yourStatus: 'P',
+        }
+    }
+
+    signUp = () => {
+        this._validateInputs(); 
+  
+        if(this.getErrorMessages().length == 0) {
+          this.props.navigation.navigate('ConfirmSignUp')
         }
     }
 
@@ -27,6 +36,16 @@ export default class SignUp extends React.Component {
         this.setState(state);
     }
 
+    _validateInputs() {
+        // Call ValidationComponent validate method
+        this.validate({
+            givenName: {required: true, minlength: 3, maxlength: 15},
+            surname: {required: true, minlength: 3, maxlength: 15},
+            mobileNumber: {required: true, numbers: true, minlength: 10, maxlength: 10},
+            referalId: {required: true, numbers: true, minlength: 6, maxlength: 6},
+        });
+    }
+
     render() {
         return (
             <SafeAreaView  style={pageStyles.container}>
@@ -34,32 +53,76 @@ export default class SignUp extends React.Component {
                     <View style={pageStyles.body}>
                         <AppTextInput
                             value={this.state.givenName}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'givenName')}
+                            onChangeText={(givenName) => {
+                                this.setState({ givenName },
+                                    () => {
+                                        this.validate({
+                                            givenName: { required: true, minlength: 3, maxlength: 15 },
+                                        })
+                                      }
+                                    )                                    
+                                }
+                            }
                             leftIcon="face"
                             placeholder="Enter First names"
                             autoCapitalize="words"
                             keyboardType="default"
                             textContentType="givenName"
                         />
+                        {this.isFieldInError('givenName') 
+                            && this.getErrorsInField('givenName').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }
                         <AppTextInput
                             value={this.state.surname}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'surname')}
+                            onChangeText={(surname) => {
+                                this.setState({ surname },
+                                    () => {
+                                        this.validate({
+                                            surname: { required: true, minlength: 3, maxlength: 15 },
+                                        })
+                                      }
+                                    )                                    
+                                }
+                            }
                             leftIcon="face"
                             placeholder="Enter Surname"
                             autoCapitalize="words"
                             keyboardType="default"
                             textContentType="familyName"
                         />
-                                            
+                        {this.isFieldInError('surname') 
+                            && this.getErrorsInField('surname').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }                 
                         <AppTextInput
                             value={this.state.mobileNumber}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'mobileNumber')}
+                            onChangeText={(mobileNumber) => {
+                                this.setState({ mobileNumber },
+                                    () => {
+                                        this.validate({
+                                            mobileNumber: { required: true, numbers: true, minlength: 10, maxlength: 10 },
+                                        })
+                                      }
+                                    )                                    
+                                }
+                            }
                             leftIcon="file-code"
                             placeholder="Enter mobile number"
                             autoCapitalize="none"
                             keyboardType="phone-pad"
                             textContentType="telephoneNumber"
                         />
+                        {this.isFieldInError('mobileNumber') 
+                            && this.getErrorsInField('mobileNumber').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }
                         <RadioButton.Group onValueChange={(val) => this.inputValueUpdate(val, 'yourStatus')} value={this.state.yourStatus}>
                             <View style={styles.buttonParallel}>
                                 <RadioButton value="P" />
@@ -72,16 +135,31 @@ export default class SignUp extends React.Component {
                         </RadioButton.Group>
                         <AppTextInput
                             value={this.state.referalId}
-                            onChangeText={(val) => this.inputValueUpdate(val, 'referalId')}
+                            onChangeText={(referalId) => {
+                                this.setState({ referalId },
+                                    () => {
+                                        this.validate({
+                                            referalId: { required: true, numbers: true, minlength: 6, maxlength: 6 },
+                                        })
+                                      }
+                                    )                                    
+                                }
+                            }
                             leftIcon="file-code"
                             placeholder="Enter Referral ID"
                             autoCapitalize="none"
                             keyboardType="phone-pad"
                             textContentType="creditCardNumber"
                         />
+                        {this.isFieldInError('referalId') 
+                            && this.getErrorsInField('referalId').map(errorMessage => 
+                            <Text style={styles.errorMsgText}>
+                                {errorMessage}
+                            </Text>) 
+                        }
                         <View style={styles.buttonContainer}>    
                             <AppButton title="Confirm" 
-                                onPress={() => this.props.navigation.navigate('ConfirmSignUp')} />
+                                onPress={() => this.signUp() } />
                         </View>
                     </View>
                     <View style={pageStyles.footer}>
@@ -107,5 +185,20 @@ const styles = StyleSheet.create({
         fontFamily: 'Arial',
         fontSize: 16,
         color: '#808080'
-    }
+    },
+    errorMsgText: {
+        fontFamily: 'Arial',
+        color: 'red',
+        fontSize: 14,
+        marginLeft: 15,
+        marginBottom: 10,
+    },
+    errorImage: {
+        width: 14,
+        height: 14,
+        borderRadius: 14 / 2,
+        marginTop: 2,
+        marginRight: 5,
+        alignSelf: 'center'
+    },
 });
